6408248af545a4a4e23d31f4264bf1ae3ef46e24
SimoneCerri <simone.cerri1994@gmail.com>
2024-12-19 16:40:11 +0100

    some style to day box

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let i = 1; i <= totalDays; i++) {
         if (i % 8 === 1) {
             row = document.createElement("div");
-            row.classList.add("row", "g-3", "justify-content-between");
+            row.classList.add("row", "p-0", "justify-content-between", "align-items-center", "mx-1", "py-4");
             calendar.appendChild(row);
         }
 
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     const day25 = document.createElement("div");
-    day25.classList.add("col-12", "day");
+    day25.classList.add("col-12", "day", "mt-4");
     day25.textContent = 25;
     calendar.appendChild(day25);
 });
