5466eeb049f0af49a150ecc1ea55a489b219d05d
SimoneCerri <simone.cerri1994@gmail.com>
2024-12-19 17:21:23 +0100

    hover and more

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const calendar = document.getElementById("calendar");
     const totalDays = 24;
 
+    const icons = [
+        'ico-albero.png',
+        'ico-calza.png',
+        'ico-campana.png',
+        'ico-caramella.png',
+        'ico-fiocco.png',
+        'ico-neve.png',
+        'ico-omino.png',
+        'ico-pupazzo.png',
+        'ico-stella.png'
+    ];
+
     let row;
     for (let i = 1; i <= totalDays; i++) {
         if (i % 8 === 1) {
@@ -12,14 +24,36 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const day = document.createElement("div");
         day.classList.add("col-lg-1", "col-md-2", "col-4", "day");
-        day.textContent = i;
+
+        const iconIndex = (i - 1) % icons.length;
+        const iconName = icons[iconIndex];
+
+        const dayIcon = document.createElement("div");
+        dayIcon.classList.add("day-icon");
+        dayIcon.innerHTML = `<img src="assets/img/icons/${iconName}" alt="icon ${i}" class="img-fluid">`;
+        day.appendChild(dayIcon);
+
+        const dayNumber = document.createElement("div");
+        dayNumber.classList.add("day-number");
+        dayNumber.textContent = i;
+        day.appendChild(dayNumber);
 
         row.appendChild(day);
     }
 
 
     const day25 = document.createElement("div");
-    day25.classList.add("col-12", "day", "mt-4");
-    day25.textContent = 25;
+    day25.classList.add("col-12", "day-25");
+
+    const day25Icon = document.createElement("div");
+    day25Icon.classList.add("day-25-icon");
+    day25Icon.innerHTML = `<img src="assets/img/page-icon.webp" alt="icon 25" class="img-fluid">`;
+    day25.appendChild(day25Icon);
+
+    const day25Number = document.createElement("div");
+    day25Number.classList.add("day-25-number");
+    day25Number.textContent = 25;
+    day25.appendChild(day25Number);
+
     calendar.appendChild(day25);
 });
